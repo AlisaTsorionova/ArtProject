c99bc7d231d06e1a8c8a231737b98dd8cea1e9ef
Алиса <tsorionovaalisa@mail.ru>
2023-05-05 23:17:18 +0300

    reg/log raw

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const regRouter = require('./Routes/regRouter');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json({ extended: true }));
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({
-  credentials: true,
-  origin: true,
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  }),
+);
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/auth', regRouter);
 
 app.listen(PORT, () => console.log(`Server has started on PORT ${PORT}`));
