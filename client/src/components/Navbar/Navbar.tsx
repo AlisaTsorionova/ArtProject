ca514b7750262328c9f2dda60d0bd669501b4941
Алиса <tsorionovaalisa@mail.ru>
2023-05-06 17:50:09 +0300

    reg/log raw html

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Navbar.scss'
+import './Navbar.scss';
 
 function Navbar(): JSX.Element {
   return (
-    <div className='container'>
+    <div className="container">
       <div>
-        <a href="/" className='logo' >NAZVANIE</a>
-        </div>
-      <div className='links'>
-      <NavLink className='link' to="/register"> Регистрация</NavLink>
-      <NavLink className='link' to="/login"> Вход</NavLink>
+        <a href="/" className="logo">
+          NAZVANIE
+        </a>
+      </div>
+      <div className="links">
+        <NavLink className="link" to="/register">
+          {' '}
+          Регистрация
+        </NavLink>
+        <NavLink className="link" to="/login">
+          {' '}
+          Вход
+        </NavLink>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar;
